dff34ea6479899de986c10dabe2e1f5643a27105
s0mnaths <somnathsharmaji05@gmail.com>
2021-09-12 06:25:00 +0530

    Create randomizing fn & update models
    Added the randomzing fns in shortener/utils.py & added a function in models.py which calls the randomizing fn if short_url field is empty.

--- shortener/models.py
+++ shortener/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .utils import create_shortened_url
 
 class Shortener(models.Model):
 
@@ -12,4 +13,10 @@ class Shortener(models.Model):
 
     def __str__(self):
         return f'{self.long_url} to {self.short_url}'
+        
+    def save(self, *args, **kwargs):
+        if not self.short_url:
+            self.short_url = create_shortened_url(self)
+
+        super().save(*args, **kwargs)
         
